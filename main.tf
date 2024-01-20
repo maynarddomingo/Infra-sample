e998cb9ed01dc752d7bad794da408a3d49d3b40a
maynarddomingo <156567493+maynarddomingo@users.noreply.github.com>
2024-01-19 21:17:55 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,3 @@
 provider "aws" {
     region = "us-east-1"  
 }
-
-resource "aws_instance" "1one" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
-  instance_type = "t2.micro"
-  tags = {
-      Name = "TF-Instance"
-  }
-}
-
-resource "aws_instance" "two" {
-  ami           = "ami-0c7217cdde317cfec" # us-west-2
-  instance_type = "t2.micro"
-  tags = {
-      Name = "TF-Instance1"
-  }
-}
