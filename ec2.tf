937627835043153dfdd8e1f323c2bff74330b9b5
maynarddomingo <156567493+maynarddomingo@users.noreply.github.com>
2024-01-19 21:17:37 -0800

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "1one" {
+  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  instance_type = "t2.micro"
+  tags = {
+      Name = "TF-Instance"
+  }
+}
+
+resource "aws_instance" "two" {
+  ami           = "ami-0c7217cdde317cfec" # us-west-2
+  instance_type = "t2.micro"
+  tags = {
+      Name = "TF-Instance1"
+  }
+}
