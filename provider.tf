6ee8001c8bfc23f2204a48f29d065b6c68fecd1e
maynarddomingo <156567493+maynarddomingo@users.noreply.github.com>
2024-02-03 15:51:43 +0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,11 +9,11 @@ terraform {
     bucket                  = "maynarddomingotfstatebucket"
     key                     = "my-terraform-project"
     region                  = "us-east-1"
-    shared_credentials_file = "~/.aws/credentials"
+//    shared_credentials_file = "~/.aws/credentials"
   }
 }
 
 provider "aws" {
   region                  = "us-east-1"
-  shared_credentials_file = "~/.aws/credentials"
+//  shared_credentials_file = "~/.aws/credentials"
 }
