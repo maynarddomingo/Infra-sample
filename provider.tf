afc317121e4ca27069d174abd309a9e1bd1b73d5
maynarddomingo <156567493+maynarddomingo@users.noreply.github.com>
2024-02-03 15:33:19 +0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,7 +6,7 @@
 
 terraform {
   backend "s3" {
-    bucket                  = "terraform-s3-state-0223"
+    bucket                  = "maynarddomingotfstatebucket"
     key                     = "my-terraform-project"
     region                  = "us-east-1"
 //    shared_credentials_file = "~/.aws/credentials"
