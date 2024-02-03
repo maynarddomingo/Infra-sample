96870a0cabf4b3179701d80cd43aaafaf4b3c409
maynarddomingo <156567493+maynarddomingo@users.noreply.github.com>
2024-02-03 15:29:03 +0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,18 @@
 provider "aws" {
     region = "us-east-1"  
 }
+
+
+terraform {
+  backend "s3" {
+    bucket                  = "terraform-s3-state-0223"
+    key                     = "my-terraform-project"
+    region                  = "us-east-1"
+    shared_credentials_file = "~/.aws/credentials"
+  }
+}
+
+provider "aws" {
+  region                  = "us-east-1"
+  shared_credentials_file = "~/.aws/credentials"
+}
